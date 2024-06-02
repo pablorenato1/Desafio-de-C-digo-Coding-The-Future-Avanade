5cb5090c069b6dd27f6f6a49543559b7f8e3e688
Pablo Renato <pablorenato18@gmail.com>
2024-06-02 00:48:35 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lista para armazenar os itens
+        List<string> itens = new List<string>();
+
+        // TODO: Solicite os itens ao usuário
+        for (int i = 0; i < 3; i++)
+        {
+            itens.Add(Console.ReadLine());
+        }
+
+        // Exibe a lista de itens
+        Console.WriteLine("Lista de itens:");
+        foreach (string item in itens)
+        {
+            Console.WriteLine($"- {item}");
+        }
+    }
+}
+
+
