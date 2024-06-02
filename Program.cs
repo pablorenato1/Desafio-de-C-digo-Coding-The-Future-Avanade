731e189a326b8d36dfd5fe0adc46950d216ab4e6
Pablo Renato <pablorenato18@gmail.com>
2024-06-02 01:29:46 -0300

    desafio completo e aceito

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+
+class Program
+{
+    static bool VerificarAtributo(string atributo, int valorMinimo, int valorMaximo, int valorAtributo)
+    {
+        //TODO: Implemente a função VerificarAtributo
+        if (valorAtributo > valorMinimo && valorAtributo < valorMaximo)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+
+    static void Main(string[] args)
+    {
+        string atributo = Console.ReadLine();
+
+        int valorMinimo = int.Parse(Console.ReadLine());
+
+        int valorMaximo = int.Parse(Console.ReadLine());
+
+        int valorAtributo = int.Parse(Console.ReadLine());
+
+        bool temp = VerificarAtributo(atributo, valorMinimo, valorMaximo, valorAtributo);
+
+        Console.WriteLine($"O valor do atributo está " + (temp ? "dentro" : "fora") + " do intervalo especificado");
+
+    }
+}
