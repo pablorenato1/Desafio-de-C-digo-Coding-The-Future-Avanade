b06b66b682d16810e6d0e417a21f5d44e5f80b4e
Pablo Renato <pablorenato18@gmail.com>
2024-06-02 01:11:52 -0300

    desafio completo e aceito

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+
+class Program
+{
+    static int CalcularDano(int ataque, int defesa)
+    {
+        int dano = ataque - defesa;
+        if (dano < 0)
+        {
+            return 0;
+        }
+        return dano;
+    }
+
+    static void Main(string[] args)
+    {
+        int ataque = int.Parse(Console.ReadLine());
+
+        int defesa = int.Parse(Console.ReadLine());
+
+        int danoCausado = CalcularDano(ataque, defesa);
+        Console.WriteLine("O dano causado pelo ataque foi: " + danoCausado);
+    }
+}
